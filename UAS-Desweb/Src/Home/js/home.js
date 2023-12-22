4f17f0e19d7708135f2394bab3e3d0501870cb17
Refa Hilyah <refahilyaa@student.uns.ac.id>
2023-12-22 19:21:04 +0700

    perjalanan saya syudah (gimik)

--- UAS-Desweb/Src/Home/js/home.js
+++ UAS-Desweb/Src/Home/js/home.js
@@ -1,28 +1,39 @@
 $(document).ready(function() {
+    // Mengaktifkan tab Bootstrap saat tab di-klik
     $('#myTabs a').click(function(e) {
         e.preventDefault();
         $(this).tab('show');
     });
-});
 
-
-$(document).ready(function(){
+    // Mengaktifkan carousel pada testimonial
     $('#testimonialsCarousel').carousel();
-});
 
-$(document).ready(function() {
+    // Memberikan efek transparan pada header saat scrolling
     var header = $("#header");
     var scrollPosition = $(window).scrollTop();
 
-    // Check scroll position on scroll
+    // Cek posisi scroll saat di-scroll
     $(window).scroll(function() {
-      scrollPosition = $(window).scrollTop();
+        scrollPosition = $(window).scrollTop();
 
-      // Add or remove the 'scrolled' class based on scroll position
-      if (scrollPosition > 0) {
-        header.addClass("scrolled");
-      } else {
-        header.removeClass("scrolled");
-      }
+        // Tambah atau hapus kelas 'scrolled' berdasarkan posisi scroll
+        if (scrollPosition > 0) {
+            header.addClass("scrolled");
+        } else {
+            header.removeClass("scrolled");
+        }
     });
-  });
+});
+
+// Fungsi pencarian perjalanan
+function cariPerjalanan() {
+    // Mendapatkan nilai dari input kode booking dan nama belakang
+    var kodeBooking = document.getElementById("kodeBooking").value;
+    var namaBelakang = document.getElementById("namaBelakang").value;
+
+    // Lakukan operasi pencarian atau tampilkan informasi perjalanan sesuai kebutuhan
+    var hasilPencarian = "Hasil pencarian untuk Kode Booking: " + kodeBooking + ", Nama Belakang: " + namaBelakang;
+    
+    // Tampilkan hasil di dalam div dengan ID "hasilPerjalanan"
+    document.getElementById("hasilPerjalanan").innerHTML = hasilPencarian;
+}
