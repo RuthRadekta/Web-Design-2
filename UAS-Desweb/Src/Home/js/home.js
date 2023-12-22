82dc2525a51fafcbd7f86e92135652cb8e03eb99
Refa Hilyah <refahilyaa@student.uns.ac.id>
2023-12-22 17:48:45 +0700

    transparansi header

--- UAS-Desweb/Src/Home/js/home.js
+++ UAS-Desweb/Src/Home/js/home.js
@@ -10,3 +10,19 @@ $(document).ready(function(){
     $('#testimonialsCarousel').carousel();
 });
 
+$(document).ready(function() {
+    var header = $("#header");
+    var scrollPosition = $(window).scrollTop();
+
+    // Check scroll position on scroll
+    $(window).scroll(function() {
+      scrollPosition = $(window).scrollTop();
+
+      // Add or remove the 'scrolled' class based on scroll position
+      if (scrollPosition > 0) {
+        header.addClass("scrolled");
+      } else {
+        header.removeClass("scrolled");
+      }
+    });
+  });
